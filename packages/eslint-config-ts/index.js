0288d2ec73a450e7bfb23d925d1c0112e1391c4f
Alice Rose Murphy <alice@acolorbright.com>
2022-02-21 11:44:52 +0100

    chore: convert spaces to tabs

--- packages/eslint-config-ts/index.js
+++ packages/eslint-config-ts/index.js
@@ -1,19 +1,19 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  env: {
-    browser: true,
-    commonjs: true,
-    es6: true,
-    es2020: true,
-    jest: true,
-    node: true,
-  },
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-  },
-  extends: [
-    '@acolorbright/eslint-config',
-    './rules/typescript',
-  ].map(require.resolve),
+	parser: '@typescript-eslint/parser',
+	env: {
+		browser: true,
+		commonjs: true,
+		es6: true,
+		es2020: true,
+		jest: true,
+		node: true,
+	},
+	parserOptions: {
+		ecmaVersion: 2020,
+		sourceType: 'module',
+	},
+	extends: [
+		'@acolorbright/eslint-config',
+		'./rules/typescript',
+	].map(require.resolve),
 };
