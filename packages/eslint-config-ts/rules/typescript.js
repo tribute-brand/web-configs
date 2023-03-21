ef5abbc5402404616a6522323104652788395aae
iksi <jurriaan@iksi.eu>
2023-03-21 09:44:49 +0100

    chore: update eslint typescript config

--- packages/eslint-config-ts/rules/typescript.js
+++ packages/eslint-config-ts/rules/typescript.js
@@ -2,8 +2,11 @@ module.exports = {
 	plugins: ['@typescript-eslint'],
 	extends: ['plugin:@typescript-eslint/recommended'],
 	rules: {
-		'@typescript-eslint/no-unused-vars': ['error'],
-		'@typescript-eslint/no-use-before-define': ['error'],
+		'@typescript-eslint/no-shadow': 'error',
+		'@typescript-eslint/no-unused-vars': 'error',
+		'@typescript-eslint/no-use-before-define': 'error',
+		'no-shadow': 'off',
+		'no-unused-vars': 'off',
 		'no-use-before-define': 'off',
 	},
 };
