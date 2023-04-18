56a65e64c3182ae2e291659a82e577dbe9f5c0b2
iksi <jurriaan@iksi.eu>
2023-04-18 17:09:52 +0200

    chore: expand browser compatibility

--- packages/browserslist-config/index.js
+++ packages/browserslist-config/index.js
@@ -1,9 +1,13 @@
 module.exports = [
-	"last 2 chrome versions",
-	"last 2 firefox versions",
-	"last 2 safari major versions",
-	"last 2 edge versions",
+	"last 2 Android versions",
+	"last 2 Chrome versions",
+	"last 2 ChromeAndroid versions",
+	"last 2 Edge versions",
+	"last 2 Firefox versions",
+	"last 2 FirefoxAndroid versions",
 	"last 2 iOS major versions",
-	"last 2 android versions",
-	"last 2 samsung versions",
+	"last 2 Opera versions",
+	"last 2 Safari major versions",
+	"last 2 Samsung versions",
+	"not dead"
 ];
