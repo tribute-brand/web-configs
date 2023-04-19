e02f2e2dd384b2e2e50db2990006c5f7ddd64e1c
iksi <jurriaan@iksi.eu>
2023-04-19 14:56:02 +0200

    chore: update browserslist configuration

--- packages/browserslist-config/index.js
+++ packages/browserslist-config/index.js
@@ -1,13 +1,7 @@
 module.exports = [
-	"last 2 Android versions",
-	"last 2 Chrome versions",
-	"last 2 ChromeAndroid versions",
-	"last 2 Edge versions",
-	"last 2 Firefox versions",
-	"last 2 FirefoxAndroid versions",
+	"last 2 versions",
 	"last 2 iOS major versions",
-	"last 2 Opera versions",
 	"last 2 Safari major versions",
-	"last 2 Samsung versions",
+	"not OperaMini all",
 	"not dead"
 ];
