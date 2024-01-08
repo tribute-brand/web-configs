4634b9d5d5cce229da7109e480730d562b5c82f9
tomislav-s <slovenec.tomislav@gmail.com>
2024-01-08 14:40:08 +0100

    feat: ignore rest siblings for unused vars

--- packages/eslint-config-ts/rules/typescript.js
+++ packages/eslint-config-ts/rules/typescript.js
@@ -1,12 +1,15 @@
 module.exports = {
-	plugins: ['@typescript-eslint'],
-	extends: ['plugin:@typescript-eslint/recommended'],
+	plugins: ["@typescript-eslint"],
+	extends: ["plugin:@typescript-eslint/recommended"],
 	rules: {
-		'@typescript-eslint/no-shadow': 'error',
-		'@typescript-eslint/no-unused-vars': 'error',
-		'@typescript-eslint/no-use-before-define': 'error',
-		'no-shadow': 'off',
-		'no-unused-vars': 'off',
-		'no-use-before-define': 'off',
+		"@typescript-eslint/no-shadow": "error",
+		"@typescript-eslint/no-unused-vars": [
+			"error",
+			{ ignoreRestSiblings: true },
+		],
+		"@typescript-eslint/no-use-before-define": "error",
+		"no-shadow": "off",
+		"no-unused-vars": "off",
+		"no-use-before-define": "off",
 	},
 };
